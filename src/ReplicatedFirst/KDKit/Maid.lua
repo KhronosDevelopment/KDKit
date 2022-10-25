48a4cea0412e1951180fc7e9b39fb4b260a80bfb
KhronosDevelopment <108852550+KhronosDevelopment@users.noreply.github.com>
2022-10-24 21:32:07 -0700

    dont use indices with Maid

--- src/ReplicatedFirst/KDKit/Maid.lua
+++ src/ReplicatedFirst/KDKit/Maid.lua
@@ -13,7 +13,9 @@ local Class = require(script.Parent:WaitForChild("Class"))
 local Maid = Class.new("KDKit.Maid")
 
 function Maid.static:isTaskValid(task)
-    if Utils:callable(Utils:getattr(task, "Destroy")) then
+    if Utils:callable(Utils:getattr(task, "clean")) then
+        return true
+    elseif Utils:callable(Utils:getattr(task, "Destroy")) then
         return true
     elseif Utils:callable(Utils:getattr(task, "Disconnect")) then
         return true
@@ -25,76 +27,52 @@ function Maid.static:isTaskValid(task)
 end
 
 function Maid:__init()
-    self.nextTaskIndex = 1
-    self.tasks = table.create(8)
+    self.tasks = table.create(32)
 end
 
 function Maid:give(task)
-    local thisTaskIndex = self.nextTaskIndex
-
     if not Maid:isTaskValid(task) then
         error(("Invalid task `%s`"):format(Utils:repr(task)))
     end
 
-    self.tasks[thisTaskIndex] = task
-
-    self.nextTaskIndex += 1
-    return thisTaskIndex
+    self.tasks[task] = true
+    return task
 end
 
-function Maid:clean(taskOrIndex)
-    if taskOrIndex == nil then
-        for index, task in self.tasks do
-            self:clean(index)
+function Maid:clean(task)
+    if task == nil then
+        for task in self.tasks do
+            self:clean(task)
         end
         return
     end
 
-    local index = taskOrIndex
-    local task = self.tasks[index]
-    if not task then
-        for potentialIndex, potentialTask in self.tasks do
-            if potentialTask == taskOrIndex then
-                index = potentialIndex
-                task = potentialTask
-                break
-            end
-        end
-
-        if not task then
-            warn(
-                ("This Maid never received or already cleaned the task `%s`. Doing nothing."):format(
-                    Utils:repr(taskOrIndex)
-                )
-            )
-            return
-        end
+    if not self.tasks[task] then
+        warn(("This Maid never received or already cleaned the task `%s`. Doing nothing."):format(Utils:repr(task)))
+        return
     end
+    self.tasks[task] = nil
 
-    self.tasks[index] = nil
+    local taskRepr = Utils:repr(task) -- the task may be a mutable table, so want to cache this in case of error
+    local s, r = xpcall(
+        coroutine.wrap(function()
+            if Utils:callable(Utils:getattr(task, "clean")) then
+                task:clean()
+            elseif Utils:callable(Utils:getattr(task, "Destroy")) then
+                task:Destroy()
+            elseif Utils:callable(Utils:getattr(task, "Disconnect")) then
+                task:Disconnect()
+            elseif Utils:callable(task) then
+                task()
+            else
+                error("Failed to resolve task cleaning method.")
+            end
+        end),
+        debug.traceback
+    )
 
-    if not Maid:isTaskValid(task) then
-        warn(("Maid can no longer clean the task `%s`. Doing nothing."):format(Utils:repr(task)))
-    else
-        local taskRepr = Utils:repr(task) -- the task may be a mutable table, so want to cache this in case of error
-        local s, r = xpcall(
-            coroutine.wrap(function()
-                if Utils:callable(Utils:getattr(task, "Destroy")) then
-                    task:Destroy()
-                elseif Utils:callable(Utils:getattr(task, "Disconnect")) then
-                    task:Disconnect()
-                elseif Utils:callable(task) then
-                    task()
-                else
-                    error("Failed to resolve task cleaning method.")
-                end
-            end),
-            debug.traceback
-        )
-
-        if not s then
-            warn(("Maid failed to clean task `%s` due to callback error:\n%s"):format(taskRepr, r))
-        end
+    if not s then
+        warn(("Maid failed to clean task `%s` due to callback error:\n%s"):format(taskRepr, r))
     end
 end
 Maid.Destroy = Maid.clean
