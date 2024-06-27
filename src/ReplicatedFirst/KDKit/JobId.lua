b5e1c79acd43fb225729a462a07b9bed0f0a1951
KhronosDevelopment <108852550+KhronosDevelopment@users.noreply.github.com>
2024-06-27 15:25:53 -0700

    add typing to JobId

--- src/ReplicatedFirst/KDKit/JobId.lua
+++ src/ReplicatedFirst/KDKit/JobId.lua
@@ -1,3 +1,5 @@
+--!strict
+
 local RunService = game:GetService("RunService")
 local KDRandom = require(script.Parent:WaitForChild("Random"))
 
@@ -5,24 +7,24 @@ local KDRandom = require(script.Parent:WaitForChild("Random"))
 local instance
 if RunService:IsServer() then
     instance = Instance.new("StringValue")
-    instance.Name = "KDKit.JobId"
+    instance.Name = "_KDKit.JobId"
 
     if RunService:IsStudio() then
         -- game.JobId is an empty string
         -- so just generate a new one
-        instance.Value = "RobloxStudio_" .. KDRandom:uuid(32)
+        instance.Value = "RobloxStudio_" .. KDRandom.uuid(32)
     else
         -- roblox job ids are occasionally reused
         -- I submitted a bug report about this with
         -- with sufficient proof and logging info,
         -- and am awaiting a fix.
         -- for now, just make it unique by adding some extra stuff
-        instance.Value = game.JobId .. "_" .. KDRandom:uuid(8)
+        instance.Value = game.JobId .. "_" .. KDRandom.uuid(8)
     end
 
     instance.Parent = game:GetService("ReplicatedStorage")
 else
-    instance = game:GetService("ReplicatedStorage"):WaitForChild("KDKit.JobId")
+    instance = game:GetService("ReplicatedStorage"):WaitForChild("_KDKit.JobId")
 end
 
 return instance.Value
