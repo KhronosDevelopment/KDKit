f9f4862c802b2a3ccc4164504bc8f563e0668d45
KhronosDevelopment <108852550+KhronosDevelopment@users.noreply.github.com>
2024-06-27 17:15:47 -0700

    add typing to Preload

--- src/ReplicatedFirst/KDKit/Preload.lua
+++ src/ReplicatedFirst/KDKit/Preload.lua
@@ -1,3 +1,5 @@
+--!strict
+
 --[[
     A group of utility modules based around loading content.
 ]]
@@ -8,19 +10,21 @@ local WARNING_INTERVAL = 5
 
 -- wait a certain number of seconds for an event to occur
 -- returns nil if the event did not return in time
-function Preload:waitForEvent(event, timeout)
+function Preload.waitForEvent(event: RBXScriptSignal, timeout: number): (boolean, any)
     local timeoutAt = os.clock() + timeout
     local returnValue = nil
 
-    local connection
+    local connection: RBXScriptConnection?
     connection = event:Connect(function(...)
         if returnValue then
             return
         end
 
         returnValue = { ... }
-        connection:Disconnect()
-        connection = nil
+        if connection then
+            connection:Disconnect()
+            connection = nil
+        end
     end)
 
     while not returnValue and os.clock() < timeoutAt do
@@ -28,26 +32,28 @@ function Preload:waitForEvent(event, timeout)
     end
 
     if returnValue then
-        return table.unpack(returnValue)
-    else
+        return true, table.unpack(returnValue)
+    elseif connection then
         connection:Disconnect()
-        return nil
     end
+
+    return false
 end
 
 -- Waits for an instance to have a certain number of children, which can be specified as the second
 -- argument but is by default pulled from the `n` attribute on the instance.
-function Preload:ensureChildren(instance, n)
+function Preload.ensureChildren(instance: Instance, n: number?): Instance
     n = n or instance:GetAttribute("n")
-    local ref = ("KDKit.Preload:ensureChildren(%s)"):format(instance:GetFullName())
+    local ref = ("KDKit.Preload.ensureChildren(%s)"):format(instance:GetFullName())
 
     if not n then
         warn(("Attempted to %s without an `n` attribute. Skipping preload."):format(ref))
         return instance
     end
+    assert(n)
 
     while #instance:GetChildren() ~= n do
-        if not self:waitForEvent(instance.ChildAdded, WARNING_INTERVAL) then
+        if not Preload.waitForEvent(instance.ChildAdded, WARNING_INTERVAL) then
             warn(
                 ("%s is waiting for %d children, but it looks like there are actually %d children."):format(
                     ref,
@@ -62,9 +68,9 @@ function Preload:ensureChildren(instance, n)
 end
 
 -- identical to Preload:ensureDescendants, except it counts the descendants and uses the `N` attribute (uppercase!)
-function Preload:ensureDescendants(instance, N)
+function Preload.ensureDescendants(instance: Instance, N: number?): Instance
     N = N or instance:GetAttribute("N")
-    local ref = ("KDKit.Preload:ensureChildren(%s)"):format(instance:GetFullName())
+    local ref = ("KDKit.Preload.ensureChildren(%s)"):format(instance:GetFullName())
 
     if not N then
         warn(("Attempted to %s without an `N` attribute. Skipping preload."):format(ref))
@@ -72,7 +78,7 @@ function Preload:ensureDescendants(instance, N)
     end
 
     while #instance:GetDescendants() ~= N do
-        if not self:waitForEvent(instance.DescendantAdded, WARNING_INTERVAL) then
+        if not Preload.waitForEvent(instance.DescendantAdded, WARNING_INTERVAL) then
             warn(
                 ("%s is waiting for %d descendants, but it looks like there are actually %d descendants."):format(
                     ref,
