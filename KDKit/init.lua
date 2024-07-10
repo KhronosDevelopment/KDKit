b087d9a4e91cb06851679c1154c032ff81d99300
KhronosDevelopment <108852550+KhronosDevelopment@users.noreply.github.com>
2024-07-10 15:51:01 -0700

    reduce accuracy of KDKit typing to improve usability

--- KDKit/init.lua
+++ KDKit/init.lua
@@ -1,21 +1,27 @@
 --!strict
 local RunService = game:GetService("RunService")
 
-return {
+local KDKit = {
     Assembly = require(script:WaitForChild("Assembly")),
     Cooldown = require(script:WaitForChild("Cooldown")),
     Cryptography = require(script:WaitForChild("Cryptography")),
-    GUI = if RunService:IsClient() then require(script:WaitForChild("GUI")) else nil,
     Humanize = require(script:WaitForChild("Humanize")),
     JobId = require(script:WaitForChild("JobId")),
     Maid = require(script:WaitForChild("Maid")),
     Mixpanel = require(script:WaitForChild("Mixpanel")),
-    Mouse = if RunService:IsClient() then require(script:WaitForChild("Mouse")) else nil,
     Mutex = require(script:WaitForChild("Mutex")),
     Preload = require(script:WaitForChild("Preload")),
     Random = require(script:WaitForChild("Random")),
     ReplicatedValue = require(script:WaitForChild("ReplicatedValue")),
-    Requests = if RunService:IsServer() then require(script:WaitForChild("Requests")) else nil,
     Time = require(script:WaitForChild("Time")),
     Utils = require(script:WaitForChild("Utils")),
 }
+
+if RunService:IsServer() then
+    KDKit.Requests = require(script:WaitForChild("Requests"))
+else
+    KDKit.GUI = require(script:WaitForChild("GUI"))
+    KDKit.Mouse = require(script:WaitForChild("Mouse"))
+end
+
+return KDKit
