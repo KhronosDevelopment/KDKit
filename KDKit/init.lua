ba6c758fdc2bdba1f308689fd9a6591c09297cc5
KhronosDevelopment <108852550+KhronosDevelopment@users.noreply.github.com>
2024-08-23 01:49:33 -0700

    export some types

--- KDKit/init.lua
+++ KDKit/init.lua
@@ -1,19 +1,39 @@
 --!strict
 local RunService = game:GetService("RunService")
 
+local Assembly = require(script:WaitForChild("Assembly"))
+local Cooldown = require(script:WaitForChild("Cooldown"))
+local Cryptography = require(script:WaitForChild("Cryptography"))
+local Humanize = require(script:WaitForChild("Humanize"))
+local Maid = require(script:WaitForChild("Maid"))
+local Mixpanel = require(script:WaitForChild("Mixpanel"))
+local Mutex = require(script:WaitForChild("Mutex"))
+local Preload = require(script:WaitForChild("Preload"))
+local Random = require(script:WaitForChild("Random"))
+local ReplicatedValue = require(script:WaitForChild("ReplicatedValue"))
+local Time = require(script:WaitForChild("Time"))
+local Utils = require(script:WaitForChild("Utils"))
+
+export type Assembly = Assembly.Assembly
+export type Cooldown = Cooldown.Cooldown
+export type Maid = Maid.Maid
+export type MixpanelClient = Mixpanel.Client
+export type Mutex = Mutex.Mutex
+export type ReplicatedValue = ReplicatedValue.ReplicatedValue
+
 local KDKit = {
-    Assembly = require(script:WaitForChild("Assembly")),
-    Cooldown = require(script:WaitForChild("Cooldown")),
-    Cryptography = require(script:WaitForChild("Cryptography")),
-    Humanize = require(script:WaitForChild("Humanize")),
-    Maid = require(script:WaitForChild("Maid")),
-    Mixpanel = require(script:WaitForChild("Mixpanel")),
-    Mutex = require(script:WaitForChild("Mutex")),
-    Preload = require(script:WaitForChild("Preload")),
-    Random = require(script:WaitForChild("Random")),
-    ReplicatedValue = require(script:WaitForChild("ReplicatedValue")),
-    Time = require(script:WaitForChild("Time")),
-    Utils = require(script:WaitForChild("Utils")),
+    Assembly = Assembly,
+    Cooldown = Cooldown,
+    Cryptography = Cryptography,
+    Humanize = Humanize,
+    Maid = Maid,
+    Mixpanel = Mixpanel,
+    Mutex = Mutex,
+    Preload = Preload,
+    Random = Random,
+    ReplicatedValue = ReplicatedValue,
+    Time = Time,
+    Utils = Utils,
 }
 
 if RunService:IsServer() then
